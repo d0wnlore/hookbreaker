4c4a4ab37f6e8c2ddf2a1c3b79d17d9607114921
d0wnlore <81343175+d0wnlore@users.noreply.github.com>
2023-09-02 14:40:12 +0900

    Initial commit + progress

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,139 @@
-console.log('This is the background page.');
-console.log('Put the background scripts here.');
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'loading') {
+    console.log('Tab is loading...');
+    chrome.action.setBadgeText({ text: '?' });
+    chrome.action.setBadgeBackgroundColor({ color: '#888888' });
+  } else if (changeInfo.status === 'complete') {
+    console.log('Tab finished loading. Checking conditions...');
+    if (changeInfo.url) {
+      tab.url = changeInfo.url; // Update the URL if it's provided in changeInfo
+    }
+    checkConditions(tab, setBadge);
+  }
+});
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  console.log('Switched tabs...');
+  chrome.action.setBadgeText({ text: '?' });
+  chrome.action.setBadgeBackgroundColor({ color: '#888888' });
+
+  chrome.tabs.get(activeInfo.tabId, (tab) => {
+    checkConditions(tab, setBadge);
+  });
+});
+
+function checkConditions(tab, callback) {
+  chrome.tabs.get(tab.id, function (updatedTab) {
+    console.log('Inside checkConditions for URL:', updatedTab.url);
+
+    if (
+      !updatedTab.url ||
+      !(
+        updatedTab.url.startsWith('http://') ||
+        updatedTab.url.startsWith('https://')
+      )
+    ) {
+      console.log(
+        'Exiting checkConditions due to non-http/https URL:',
+        updatedTab.url
+      );
+      return;
+    }
+
+    const domain = new URL(updatedTab.url).hostname;
+    let conditionsMet = 0;
+
+    if (domain.includes('-')) {
+      conditionsMet++;
+    }
+
+    if (domain.endsWith('.ru')) {
+      conditionsMet++;
+    }
+
+    const tokens = ['usdc', 'usdt', 'eth', 'btc'];
+
+    if (tokens.some((token) => domain.includes(token))) {
+      conditionsMet++;
+    }
+
+    const tlds = ['ru', 'gift'];
+
+    if (tlds.some((tld) => domain.includes(tld))) {
+      conditionsMet++;
+    }
+
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: updatedTab.id },
+        func: contentCheck,
+      },
+      (results) => {
+        if (chrome.runtime.lastError) {
+          console.error('Error in executeScript:', chrome.runtime.lastError);
+          return;
+        }
+
+        if (!results) {
+          console.error(
+            'No results from executeScript. Possibly blocked by Content Security Policy or another error.'
+          );
+          return;
+        }
+
+        console.log('Results from executeScript:', results);
+
+        if (results && results[0].result) {
+          conditionsMet++;
+        }
+        callback(conditionsMet);
+      }
+    );
+  });
+}
+
+function contentCheck() {
+  return document.body.innerHTML.toLowerCase().includes('airdrop');
+}
+
+function setBadge(conditionsMet) {
+  let grade;
+  switch (conditionsMet) {
+    case 0:
+      return 'A';
+    case 1:
+      return 'B';
+    case 2:
+      return 'C';
+    case 3:
+      return 'D';
+    case 4:
+      return 'F';
+    default:
+      grade = 'N/A';
+      break;
+  }
+
+  console.log('Setting badge to:', grade);
+  chrome.action.setBadgeText({ text: grade });
+  chrome.action.setBadgeBackgroundColor({
+    color: determineBadgeColor(conditionsMet),
+  });
+}
+
+function determineBadgeColor(conditionsMet) {
+  switch (conditionsMet) {
+    case 0:
+      return '#00FF00'; // Green
+    case 1:
+      return '#ADFF2F'; // Yellowish Green
+    case 2:
+      return '#FFFF00'; // Yellow
+    case 3:
+      return '#FFA500'; // Orange
+    case 4:
+      return '#FF0000'; // Red
+    default:
+      return '#888888'; // Grey
+  }
+}
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,27 +1,119 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
-import './Popup.css';
+import React, { useEffect, useState } from 'react';
+import './popup.css';
 
-const Popup = () => {
+function Popup() {
+  const [conditions, setConditions] = useState({
+    'Domain has a dash (-)': null,
+    'Domain uses a .ru, .gift TLD': null,
+    'Subdomain has a token name': null,
+    "The word 'airdrop' is found in the HTML": null,
+  });
+  const [grade, setGrade] = useState('?');
+
+  useEffect(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const domain = new URL(tabs[0].url).hostname;
+
+      // Check for dash in domain
+      conditions['Domain has a dash (-)'] = !domain.includes('-');
+
+      // Check for .ru TLD
+      const tlds = ['ru', 'gift'];
+      conditions['Domain uses a .ru, .gift TLD'] = !tlds.some((tld) =>
+        domain.includes(tld)
+      );
+
+      // Check for token name in subdomain
+      const tokens = ['usdc', 'usdt', 'eth', 'btc'];
+      conditions['Subdomain has a token name'] = !tokens.some((token) =>
+        domain.includes(token)
+      );
+
+      chrome.scripting.executeScript(
+        {
+          target: { tabId: tabs[0].id },
+          func: contentCheck,
+        },
+        (results) => {
+          conditions["The word 'airdrop' is found in the HTML"] =
+            !results[0].result;
+
+          // Calculate grade based on conditions
+          const negativeConditions = Object.values(conditions).filter(
+            (val) => val === false
+          ).length;
+          const calculatedGrade = determineGrade(negativeConditions);
+          setGrade(calculatedGrade);
+
+          // Update badge with the new grade
+          chrome.action.setBadgeText({ text: calculatedGrade });
+          chrome.action.setBadgeBackgroundColor({
+            color: determineBadgeColor(negativeConditions),
+          });
+
+          // Update state with new conditions
+          setConditions({ ...conditions });
+        }
+      );
+    });
+  }, []);
+
+  function contentCheck() {
+    return document.body.innerHTML.toLowerCase().includes('airdrop');
+  }
+
+  function determineGrade(negativeConditions) {
+    switch (negativeConditions) {
+      case 0:
+        return 'A';
+      case 1:
+        return 'B';
+      case 2:
+        return 'C';
+      case 3:
+        return 'D';
+      case 4:
+        return 'F';
+      default:
+        return '???';
+    }
+  }
+
+  function determineBadgeColor(negativeConditions) {
+    switch (negativeConditions) {
+      case 0:
+        return '#00FF00'; // Green
+      case 1:
+        return '#ADFF2F'; // Yellowish Green
+      case 2:
+        return '#FFFF00'; // Yellow
+      case 3:
+        return '#FFA500'; // Orange
+      case 4:
+        return '#FF0000'; // Red
+      default:
+        return '#888888'; // Grey
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+      <h1>Grade: {grade}</h1>
+      <p>Final grade based on:</p>
+      <ul>
+        {Object.keys(conditions).map((condition, index) => (
+          <li key={index}>
+            {condition}:{' '}
+            {conditions[condition] === null
+              ? '⏳'
+              : conditions[condition]
+              ? '✅'
+              : '🚫'}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-};
+}
 
 export default Popup;
